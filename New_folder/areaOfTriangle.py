fd2d6062bb37abbf3a840b975d39e8fa0abbee50
M, Kishore (ITDV AE) - AF (ext) <kim1-ext@airfrance.fr>
2024-09-08 12:31:54 +0530

    Update areaOfTriangle.py
    new variable for Area

--- New_folder/areaOfTriangle.py
+++ New_folder/areaOfTriangle.py
@@ -13,5 +13,6 @@ c = 7
 s = (a + b + c) / 2
 
 # calculate the area
-area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
-print('The area of the triangle is %0.2f' %area)
+area = d
+d=(s*(s-a)*(s-b)*(s-c)) ** 0.5
+print('The area of the triangle is %0.2f' %d)
